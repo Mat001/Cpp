b9ed5b00c82210882ed7370638f683d288aaeb4c
Matjaz Pirnovar <matjazpirnovar@gmail.com>
2012-01-27 23:09:51 -0800

    Update tortoise_hare.cpp

--- tortoise_hare.cpp
+++ tortoise_hare.cpp
@@ -1,7 +1,7 @@
 // ex. 8.12 tortoise and hare race
 // the point of using pass by reference with pointer arguments in this example is to directly modify values in main()
 //#include <iostream>
-#include <iomanip>
+//#include <iomanip>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
