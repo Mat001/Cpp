1af2dde28ad32aa4802da16c515d18e57957ec2b
Matjaz Pirnovar <matjazpirnovar@gmail.com>
2012-01-31 18:13:20 -0800

    added quicksort.cpp -- in progress

---
+++ quicksort.cpp
@@ -0,0 +1,29 @@
+
+// Exercise 8.15, Quicksort
+
+#include <iostream>
+using namespace std;
+
+void quicksort(int array[], const int arraySize);
+//void partition()...
+
+int main()
+{
+   const int arraySize = 10;
+   int array[arraySize] = {};
+  
+   // call quicksort function
+   quicksort();
+
+   
+}
+  
+
+   // 1. partitioning step -- sort the first element
+   void quicksort()
+   {
+
+      // 2. recursive step -- perform step 1 (partitioning step)  on each subarray.
+
+
+   }
